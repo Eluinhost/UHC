d21d15e58e8cdbaf7fc247561350a88c928b99ce
D4mnX <paulschwandes@web.de>
2016-05-06 12:14:00 +0200

    Recreate icon inventories on every show invocation (fixes #98)

--- src/main/java/gg/uhc/uhc/inventory/IconInventory.java
+++ src/main/java/gg/uhc/uhc/inventory/IconInventory.java
@@ -27,35 +27,63 @@
 
 package gg.uhc.uhc.inventory;
 
+import gg.uhc.uhc.UHCPluginDisableEvent;
+
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class IconInventory implements IconUpdateListener, Listener {
 
     protected static final int INVENTORY_WIDTH = 9;
 
     protected final List<IconStack> icons = Lists.newArrayList();
+    protected final Set<Inventory> openedInventories = Sets.newHashSet();
 
     protected final String title;
-    protected Inventory inventory;
 
     public IconInventory(String title) {
         this.title = title;
-        inventory = Bukkit.createInventory(null, INVENTORY_WIDTH, title);
     }
 
     public void showTo(HumanEntity entity) {
+        // how many icons do we want to show
+        final int iconCount = icons.size();
+
+        // how many slots are required, in increments of 9, min 9, max 54
+        final int slotsRequired = Math.max(
+                9,
+                Math.min(
+                        54,
+                        INVENTORY_WIDTH * ((iconCount + INVENTORY_WIDTH - 1) / INVENTORY_WIDTH)
+                )
+        );
+
+        // Create and render inventory
+        final Inventory inventory = Bukkit.createInventory(null, slotsRequired, title);
+        final ItemStack[] contents = new ItemStack[slotsRequired];
+        for (int i = 0; i < icons.size(); i++) {
+            contents[i] = icons.get(i);
+        }
+
+        inventory.setContents(contents);
+
+        // Show to player
         entity.openInventory(inventory);
+        openedInventories.add(inventory);
     }
 
     public void registerNewIcon(IconStack icon) {
@@ -68,14 +96,7 @@ public class IconInventory implements IconUpdateListener, Listener {
         // add item to list in the correct location
         final int index = indexToInsert(icon);
         icons.add(index, icon);
-
-        // make sure this is enough space now
-        ensureInventorySize();
-
-        // rerender all items past this one (inclusive) as they have shifted index right 1
-        for (int i = index; i < icons.size(); i++) {
-            inventory.setItem(i, icons.get(i));
-        }
+        reopenForCurrentViewers();
 
         return index;
     }
@@ -91,6 +112,22 @@ public class IconInventory implements IconUpdateListener, Listener {
         return insertionIndex;
     }
 
+    protected Collection<HumanEntity> getCurrentViewers() {
+        final List<HumanEntity> viewers = Lists.newArrayList();
+        for (final Inventory openedInventory : openedInventories) {
+            viewers.addAll(openedInventory.getViewers());
+        }
+
+        return viewers;
+    }
+
+    protected void reopenForCurrentViewers() {
+        for (final HumanEntity viewer : getCurrentViewers()) {
+            viewer.closeInventory();
+            showTo(viewer);
+        }
+    }
+
     @Override
     public void onUpdate(IconStack icon) {
         final int index = icons.indexOf(icon);
@@ -99,7 +136,9 @@ public class IconInventory implements IconUpdateListener, Listener {
         if (index < 0) return;
 
         // refresh the item
-        inventory.setItem(index, icon);
+        for (final Inventory openedInventory : openedInventories) {
+            openedInventory.setItem(index, icon);
+        }
     }
 
     @Override
@@ -112,63 +151,22 @@ public class IconInventory implements IconUpdateListener, Listener {
         // remove from the list
         icons.remove(index);
 
+        reopenForCurrentViewers();
+
         // readd to the list
         final int newIndex = add(icon);
-
-        // rerender the changed items (index+ is handled by the add() method)
-        for (int i = newIndex; i < index; i++) {
-            inventory.setItem(i, icons.get(i));
-        }
-    }
-
-    protected void ensureInventorySize() {
-        // how many icons do we want to show
-        final int iconCount = icons.size();
-
-        // how many slots are required, in increments of 9, min 9, max 54
-        final int slotsRequired = Math.max(
-                9,
-                Math.min(
-                        54,
-                        INVENTORY_WIDTH * ((iconCount + INVENTORY_WIDTH - 1) / INVENTORY_WIDTH)
-                )
-        );
-
-        // if it's already the right size then do nothing
-        if (slotsRequired == inventory.getSize()) return;
-
-        // clear the old inventory just in case
-        inventory.clear();
-
-        // create a replacement
-        final Inventory newInventory = Bukkit.createInventory(null, slotsRequired, title);
-
-        // rerender entire inventory
-        final ItemStack[] contents = new ItemStack[slotsRequired];
-        for (int i = 0; i < icons.size(); i++) {
-            contents[i] = icons.get(i);
-        }
-
-        newInventory.setContents(contents);
-
-        // replace the old inventory
-        for (final HumanEntity entity : inventory.getViewers()) {
-            entity.closeInventory();
-            entity.openInventory(newInventory);
-        }
-
-        inventory = newInventory;
     }
 
     @EventHandler(ignoreCancelled = true)
     public void on(InventoryClickEvent event) {
-        if (!event.getInventory().getTitle().equals(title)) return;
+        final Inventory inventory = event.getInventory();
+        if (!inventory.getTitle().equals(title)) return;
 
         event.setCancelled(true);
 
         final int slot = event.getRawSlot();
 
-        if (slot < 0 || slot > inventory.getSize()) return;
+        if (slot < 0 || slot >= inventory.getSize() || slot >= icons.size()) return;
 
         // call event
         final IconStack icon = icons.get(slot);
@@ -176,5 +174,19 @@ public class IconInventory implements IconUpdateListener, Listener {
         if (icon != null) {
             icon.onClick((Player) event.getWhoClicked());
         }
+    }
+
+    @EventHandler
+    public void on(InventoryCloseEvent event) {
+        openedInventories.remove(event.getInventory());
+    }
+
+    @EventHandler
+    public void on(UHCPluginDisableEvent event) {
+        for (final HumanEntity viewer : getCurrentViewers()) {
+            viewer.closeInventory();
+        }
+
+        openedInventories.clear();
     }
 }
